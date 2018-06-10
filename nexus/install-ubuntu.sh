063ef2c99272c3aa84b48f02693115192064146c
Alfred <alfred.yctan@gmail.com>
2018-06-10 23:22:45 +0800

    add nexus

---
+++ nexus/install-ubuntu.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+VERSION=${1:-3.12.0-01}
+INSTALL_DIR=${2:-"/opt/sys/vendor/nexus"}
+
+sudo groupadd nexus
+sudo useradd -g nexus nexus
+
+mkdir -p $INSTALL_DIR
+cd $INSTALL_DIR
+wget https://sonatype-download.global.ssl.fastly.net/repository/repositoryManager/3/nexus-${VERSION}-unix.tar.gz 
+tar -zxvf nexus-${VERSION}-unix.tar.gz
+rm -f nexus-${VERSION}-unix.tar.gz
+ln -s nexus-${VERSION} current
+sudo chown -R nexus:nexus $INSTALL_DIR
+
+
+sudo cp nexus.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable nexus.service
+sudo systemctl start nexus.service
+
