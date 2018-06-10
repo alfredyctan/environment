e8adfa43f48b56c80e0b1d7356e4a7bdeea0cf9a
Alfred Tan <alfred.yctan@gmail.com>
2018-06-10 19:56:56 +0800

    remote ssh

--- docker/docker.sh
+++ docker/docker.sh
@@ -4,12 +4,12 @@ USER=$2
 for HOST in `cat $ENV/hosts.cfg`
 do
 	echo "working on $HOST"
-	ssh ${USER}@${HOST} "sudo apt-get install apt-transport-https ca-certificates curl software-properties-common"
+	ssh ${USER}@${HOST} "sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common"
 	ssh ${USER}@${HOST} "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -"
 	ssh ${USER}@${HOST} "sudo apt-key fingerprint 0EBFCD88"
 	ssh ${USER}@${HOST} 'sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable'
     ssh ${USER}@${HOST} "apt-cache madison docker-ce"
-    ssh ${USER}@${HOST} "sudo apt-get install docker-ce"
+    ssh ${USER}@${HOST} "sudo apt-get install -y docker-ce"
 
 	ssh ${USER}@${HOST} "sudo usermod -G docker $USER"
 	ssh ${USER}@${HOST} "docker info"
