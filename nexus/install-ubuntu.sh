dbdb033d8768bb2d11b3c2ed8a8056b1c7219be0
Alfred <alfred.yctan@gmail.com>
2018-06-10 23:35:10 +0800

    polish

--- nexus/install-ubuntu.sh
+++ nexus/install-ubuntu.sh
@@ -3,20 +3,16 @@
 VERSION=${1:-3.12.0-01}
 INSTALL_DIR=${2:-"/opt/sys/vendor/nexus"}
 
-PWD=`pwd`
-
 sudo groupadd nexus
 sudo useradd -g nexus nexus
 
 mkdir -p $INSTALL_DIR
-cd $INSTALL_DIR
-wget https://sonatype-download.global.ssl.fastly.net/repository/repositoryManager/3/nexus-${VERSION}-unix.tar.gz 
-tar -zxvf nexus-${VERSION}-unix.tar.gz
-rm -f nexus-${VERSION}-unix.tar.gz
-ln -s nexus-${VERSION} current
+wget https://sonatype-download.global.ssl.fastly.net/repository/repositoryManager/3/nexus-${VERSION}-unix.tar.gz -P $INSTALL_DIR
+tar -zxvf $INSTALL_DIR/nexus-${VERSION}-unix.tar.gz -C $INSTALL_DIR
+rm -f $INSTALL_DIR/nexus-${VERSION}-unix.tar.gz
+ln -s nexus-${VERSION} $INSTALL_DIR/current
 sudo chown -R nexus:nexus $INSTALL_DIR
 
-cd $PWD
 sudo cp nexus.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable nexus.service
