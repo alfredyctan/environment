074a0690c2e73f2e403dbe489204d385a1d183bc
Alfred Tan <alfred.yctan@gmail.com>
2018-06-10 19:55:46 +0800

    remote ssh

--- docker/docker.sh
+++ docker/docker.sh
@@ -4,16 +4,15 @@ USER=$2
 for HOST in `cat $ENV/hosts.cfg`
 do
 	echo "working on $HOST"
-	sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-	sudo apt-key fingerprint 0EBFCD88
-	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    apt-cache madison docker-ce
-    sudo apt-get install docker-ce
+	ssh ${USER}@${HOST} "sudo apt-get install apt-transport-https ca-certificates curl software-properties-common"
+	ssh ${USER}@${HOST} "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -"
+	ssh ${USER}@${HOST} "sudo apt-key fingerprint 0EBFCD88"
+	ssh ${USER}@${HOST} 'sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable'
+    ssh ${USER}@${HOST} "apt-cache madison docker-ce"
+    ssh ${USER}@${HOST} "sudo apt-get install docker-ce"
 
-	sudo usermod -G docker $USER
-	docker info
-	docker run hello-world
-
+	ssh ${USER}@${HOST} "sudo usermod -G docker $USER"
+	ssh ${USER}@${HOST} "docker info"
+	ssh ${USER}@${HOST} "docker run hello-world"
 done
 
