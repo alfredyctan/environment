ddf6291b8c150e1f4ca87a2e79f067b2482dd4ec
Alfred Tan <alfred.yctan@gmail.com>
2018-06-10 21:34:11 +0800

    add portainer

--- portainer/portainer.sh
+++ portainer/portainer.sh
@@ -2,7 +2,7 @@
 ENV=$1
 USER=$2
 
-for HOST in `cat portainer/$ENV/hosts.cfg`
+for HOST in `cat $ENV/hosts.cfg`
 do
 	scp setup.sh ${USER}@${HOST}:/tmp
 	ssh ${USER}@${HOST} "chmod 750 /tmp/setup.sh; /tmp/setup.sh; rm /tmp/setup.sh;"
